f02db41af2ebed367b41ac0000e9c5d4963198a2
Nicolai Czempin <nicolai.czempin@gmail.com>
2017-04-05 15:00:26 +0200

    Factor out choose_move

--- examples/qtable.py
+++ examples/qtable.py
@@ -1,10 +1,12 @@
+# based on https://medium.com/emergent-future/simple-reinforcement-learning-with-tensorflow-part-0-q-learning-with-tables-and-neural-networks-d195264329d0
+# "Simple Reinforcement Learning with Tensorflow Part 0: Q-Learning with Tables and Neural Networks" by Arthur Juliani
+
+
 import gym
 import numpy as np
 import queue
 import gym_tic_tac_toe
 import random
-# based on https://medium.com/emergent-future/simple-reinforcement-learning-with-tensorflow-part-0-q-learning-with-tables-and-neural-networks-d195264329d0
-# "Simple Reinforcement Learning with Tensorflow Part 0: Q-Learning with Tables and Neural Networks" by Arthur Juliani
 
 
 def pick_min(choices, moves):
@@ -31,13 +33,13 @@ def pick_max(choices, moves):
     return best_move
 
 def hash_ttt(state):
-    #of course this is just for the upper bound;
-    #we should really take advantage of the redundancies
+    # of course this is just for the upper bound;
+    # we should really take advantage of the redundancies
     # to reduce the number of states to 765 for the board
     # and who is on move really is implicit in how many
     # squares are occupied
     
-    #print("hashing state: ", state)
+    # print("hashing state: ", state)
     retval = 0
     low9 = 0
     high9 = 0
@@ -51,7 +53,7 @@ def hash_ttt(state):
             retval |= lowmult
             if (board[i] < 0):
                 retval |= highmult
-        lowmult *=2
+        lowmult *= 2
         highmult *= 2
     return retval
 def random_plus_middle_move(moves, p):
@@ -63,101 +65,111 @@ def random_plus_middle_move(moves, p):
 def random_move(moves, p):
     m = random.choice(moves)
     return m
+def choose_move(Q, hs, env, om, maximizing_player, bonus):
+    choices = Q[hs, :] + bonus
+#        print ("choices: " , choices)
+    moves = env.move_generator()
+#        print ("legal moves: ", moves)
+    a = []
+    if len(moves) == 1:
+        a = moves[0]
+    elif len(moves) > 0:
+        if (om == maximizing_player):
+            a = pick_max(choices, moves)
+        
+#            a = [om, np.argmax(choices)]
+        else:  # om == -1
+#            a = [om, np.argmin(choices)]
+            a = pick_min(choices, moves)
+            
+    return a
 
+def train(env):
 
+    asp = env.action_space
+    print (asp)
+    
+    os = env.observation_space
+    print (os)
+    
+    
+    # Initialize table with all zeros
+    Q = np.zeros([os.n, asp.n])
+    # Set learning parameters
+    lr = .85  # learning rate
+    y = .99  # discount factor.
+    num_episodes = 10000
+    ROLLING_ELEMENTS = 1000.0
+    # create lists to contain total rewards and steps per episode
+    # jList = []
+    rList = []
+    q = queue.Queue(ROLLING_ELEMENTS)
+    for i in range(num_episodes):
+        # Reset environment and get first new observation
+        s = env.reset()
+        maximizing_player = 1  # TODO randomize; for that we need to generalize the reward function
+        rAll = 0
+        d = False
+        j = 0
+        # The Q-Table learning algorithm
+        while j < 99:
+            j += 1
+            # print (Q)
+            # Choose an action by greedily (with noise) picking from Q table
+      #         print ("s: ", s)
+    #         print ("hs: ", hs)
+            pick = np.random.randn(1, asp.n)
+            puck = (1. / (i + 1))
+            bonus = pick * puck
+     #       print ("pick: ", pick, ", puck: ", puck, ", bonus: ", bonus)
+            om = s['on_move']
+            hs = hash_ttt(s)
+            a = choose_move(Q, hs, env, om, maximizing_player, bonus)
+            if (not a):
+                break
+                # a = random_move(moves, om)
+              
+            # Get new state and reward from environment
+    #        print('action: ', a)
+            s1, reward, d, _ = env.step(a)
+            hs1 = hash_ttt(s1)
+    #        print ('new state: ', s1, " (", hs1, "), reward: ", r)
+            rAll += reward
+            # Update Q-Table with new knowledge
+            if (om == maximizing_player):
+                Q[hs, a] = Q[hs, a] + lr * (reward + y * np.max(Q[hs1, :]) - Q[hs, a])
+            else:
+                Q[hs, a] = Q[hs, a] + lr * (reward + y * np.min(Q[hs1, :]) - Q[hs, a])
+              
+            s = s1
+            if (d == True):
+              break
+        sz = q.qsize()
+        if (q.full()):
+            q.get()  # remove one item to make room
+        q.put(rAll)
+        rolling_rAll = 0
+        if(q.full()):
+            for k in range(int(ROLLING_ELEMENTS)):
+                it = q.get()
+                # print (it)
+                rolling_rAll += it 
+                q.put(it)
+            if (i % 500 == 0):
+                print (rolling_rAll / ROLLING_ELEMENTS)
+        rList.append(rAll)
+    #     if (rolling_rAll >= 9):
+    #       print ("good at ", i)
+          # break
+    
+    print ("Score over time: " + str(sum(rList) / num_episodes))
+    print ("Final Q-Table Values")
+    return Q
 
 env = gym.make('tic_tac_toe-v0')
-asp = env.action_space
-print (asp)
 
-os = env.observation_space
-print (os)
+Q = train(env)
 
-
-#Initialize table with all zeros
-Q = np.zeros([os.n,asp.n])
-# Set learning parameters
-lr = .85
-y = .99 # discount factor.
-num_episodes = 10000
-ROLLING_ELEMENTS = 1000.0
-#create lists to contain total rewards and steps per episode
-#jList = []
-rList = []
-q = queue.Queue(ROLLING_ELEMENTS)
-for i in range(num_episodes):
-    #Reset environment and get first new observation
-    s = env.reset()
-    maximizing_player = 1 #TODO randomize; for that we need to generalize the reward function
-    rAll = 0
-    d = False
-    j = 0
-    #The Q-Table learning algorithm
-    while j < 99:
-        j+=1
-        #print (Q)
-        #Choose an action by greedily (with noise) picking from Q table
-        hs = hash_ttt(s)
-#         print ("s: ", s)
-#         print ("hs: ", hs)
-        pick =  np.random.randn(1,asp.n)
-        puck = (1./(i+1))
-        bonus = pick * puck
- #       print ("pick: ", pick, ", puck: ", puck, ", bonus: ", bonus)
-        choices = Q[hs,:] +bonus
-#        print ("choices: " , choices)
-        om = s['on_move']
-        moves = env.move_generator()
-#        print ("legal moves: ", moves)
-        a = []
-        if len(moves) == 0:
-            break
-        elif len(moves) == 1:
-            a = moves[0]
-        else:
-            if (om == maximizing_player):
-                a = pick_max(choices, moves)
-            
-#            a = [om, np.argmax(choices)]
-            else: # om == -1
-#            a = [om, np.argmin(choices)]
-                a= pick_min(choices, moves)
-            
-            #a = random_move(moves, om)
-          
-        #Get new state and reward from environment
-#        print('action: ', a)
-        s1,reward,d,_ = env.step(a)
-        hs1 = hash_ttt(s1)
-#        print ('new state: ', s1, " (", hs1, "), reward: ", r)
-        rAll += reward
-        #Update Q-Table with new knowledge
-        if (om == maximizing_player):
-            Q[hs,a] = Q[hs,a] + lr*(reward + y*np.max(Q[hs1,:]) - Q[hs,a])
-        else:
-            Q[hs,a] = Q[hs,a] + lr*(reward + y*np.min(Q[hs1,:]) - Q[hs,a])
-          
-        s = s1
-        if (d == True):
-          break
-    sz = q.qsize()
-    if (q.full()):
-        q.get()#remove one item to make room
-    q.put(rAll)
-    rolling_rAll = 0
-    if(q.full()):
-        for k in range(int(ROLLING_ELEMENTS)):
-            it = q.get()
-            #print (it)
-            rolling_rAll += it 
-            q.put(it)
-        if (i % 500 == 0):
-            print (rolling_rAll/ROLLING_ELEMENTS)
-    rList.append(rAll)
-#     if (rolling_rAll >= 9):
-#       print ("good at ", i)
-      #break
-
-print ("Score over time: " +  str(sum(rList)/num_episodes))
-print ("Final Q-Table Values")
 print (Q)
+
+s = env.reset()
