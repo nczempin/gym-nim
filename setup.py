4aedd3fa9f05192e567e38dc95e070e8b86de235
Nicolai Czempin <nicolai.czempin@gmail.com>
2017-05-14 14:57:54 +0200

    Specify version for gym dependency

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(name='gym_nim',
       version='0.0.1',
-      install_requires=['gym'],
+      install_requires=['gym==0.9.1'],
       url="https://github.com/nczempin/gym-nim",
       packages=find_packages()
 )  
